5c97ffa69f5ab163f2465f6610f590e7ad35e5f4
ErCollao <daniel@collado-ruiz.es>
2019-03-13 10:14:31 +0200

    Update to rails 5 plugin - moved init logic to each file

--- lib/incomplete_date/active_record.rb
+++ lib/incomplete_date/active_record.rb
@@ -1,47 +1,55 @@
 class IncompleteDate
-
-  module ActiveRecord::ClassMethods
-
+  module IncompleteDateAttr
+    extend ActiveSupport::Concern
     #
     # Defines a new IncompleteDate virtual attribute with name +attr_name+
     # corresponding to a real integer attribute named +raw_name+ that holds the
     # raw date value in the database.
     #
-    def incomplete_date_attr(attr_name, raw_name)
-      instance_var = "@#{attr_name}"
+    class_methods do
 
-      # getter
-      define_method "#{attr_name}" do
-        value = instance_variable_get(instance_var)
-        value ||= IncompleteDate.new(read_attribute(raw_name))
-        instance_variable_set(instance_var, value)
-        value
-      end
+      def incomplete_date_attr(attr_name, raw_name)
+        instance_var = "@#{attr_name}"
 
-      # setter
-      define_method "#{attr_name}=" do |value|
-        #begin
-          value = IncompleteDate.new(value)
+        # getter
+        define_method "#{attr_name}" do
+          value = instance_variable_get(instance_var)
+          value ||= IncompleteDate.new(read_attribute(raw_name))
           instance_variable_set(instance_var, value)
-          write_attribute(raw_name, value.to_i)
-        #rescue ArgumentError
-        #  errors.add(raw_name)
-        #end
-      end
-    end
+          value
+        end
 
-    #
-    # Defines several virtual attributes at once for raw real attributes
-    #
-    def incomplete_date_attrs(*names)
-      options = names.extract_options!
-      prefix = options.fetch(:prefix, 'raw')
-      names.each do |name|
-        raw_name = "#{prefix}_#{name}"
-        incomplete_date_attr name, raw_name
+        # setter
+        define_method "#{attr_name}=" do |value|
+          #begin
+            value = IncompleteDate.new(value)
+            instance_variable_set(instance_var, value)
+            write_attribute(raw_name, value.to_i)
+          #rescue ArgumentError
+          #  errors.add(raw_name)
+          #end
+        end
       end
+
+      #
+      # Defines several virtual attributes at once for raw real attributes
+      #
+      def incomplete_date_attrs(*names)
+        options = names.extract_options!
+        prefix = options.fetch(:prefix, 'raw')
+        names.each do |name|
+          raw_name = "#{prefix}_#{name}"
+          incomplete_date_attr name, raw_name
+        end
+      end
+
     end
 
   end
+end
 
+class ApplicationRecord < ActiveRecord::Base
+  include IncompleteDate::IncompleteDateAttr
+
+  self.abstract_class = true
 end
--- lib/incomplete_date/date.rb
+++ lib/incomplete_date/date.rb
@@ -1,24 +1,22 @@
-class IncompleteDate
-  module Date::InstanceMethods
-    def circa
-      false
-    end
-    alias circa? circa
+class Date
+  def circa
+    false
+  end
+  alias circa? circa
 
-    def to_birthday
-      IncompleteDate.new(:month => month, :day => day)
-    end
+  def to_birthday
+    IncompleteDate.new(:month => month, :day => day)
+  end
 
-    def to_i
-      year*10000 + month*100 + day
-    end
+  def to_i
+    year*10000 + month*100 + day
+  end
 
-    def to_incomplete_date
-      IncompleteDate.new(self)
-    end
+  def to_incomplete_date
+    IncompleteDate.new(self)
+  end
 
-    def matches?(incomplete_date)
-      incomplete_date.include?(self)
-    end
+  def matches?(incomplete_date)
+    incomplete_date.include?(self)
   end
 end
